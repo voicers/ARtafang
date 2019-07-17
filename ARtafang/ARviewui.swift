f119318bc3da63c81857f57ebbc41586ecba45db
evans_90s <38302724+voicers@users.noreply.github.com>
2019-07-17 15:08:16 +0800

    vhnvhm

---
+++ ARtafang/ARviewui.swift
@@ -0,0 +1,30 @@
+//
+//  ARviewui.swift
+//  ARtafang
+//
+//  Created by Evan Guo on 2019/7/17.
+//  Copyright © 2019年 nxkj. All rights reserved.
+//
+
+import UIKit
+
+class ARviewui: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
